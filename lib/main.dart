07cf2975056a8e452a1c5c608377049f4cccd20f
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-10-24 00:11:12 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todolist_app/screens/home_screen.dart';
+import 'package:todolist_app/screens/home_screen/home_screen.dart';
+import 'package:todolist_app/screens/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
+      routes: {
+        '/HomeScreen': (context) => const HomeScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:todolist_app/util/app_images.dart';
+import 'package:todolist_app/util/color.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () => Navigator.popAndPushNamed(context, '/HomeScreen'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        height: screenHeight,
+        width: screenWidth,
+        color: AppColors.primary,
+        child: Center(
+          child: Image.asset(AppImages.appLogo),
+        ),
+      ),
+    );
+  }
+}
