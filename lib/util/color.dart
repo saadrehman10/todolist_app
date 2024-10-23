ccc3a79758bd2106ce4d4aba63b8d4d0ea119719
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-10-23 23:29:38 +0500

    update

--- lib/util/color.dart
+++ lib/util/color.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
 class AppColors {
-
-
-  
+  static const Color primary = Color(0xffff);
+  static const Color secondary = Color(0xff00ff); // Added secondary color
+  static const Color tertiary = Color(0xff0000ff); // Added thishery color
+  static const Color quaternary = Color.fromARGB(255, 136, 136, 136); // Added quartira color
+  static const Color quinary = Color(0xffffff00); // Added quartira color
 }
