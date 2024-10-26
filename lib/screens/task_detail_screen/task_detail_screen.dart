24ee15a317304e10affb67e42d011fcfcf1d2819
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-10-26 22:29:49 +0500

    update

--- lib/screens/task_detail_screen/task_detail_screen.dart
+++ lib/screens/task_detail_screen/task_detail_screen.dart
@@ -7,7 +7,7 @@ class TaskDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         body: SafeArea(
             child: Column(
       children: [
--- lib/util/text.dart
+++ lib/util/text.dart
@@ -3,8 +3,4 @@ class AppText {}
 class TaskDetailScreenText {
   static const String headingTitle = 'Task Title: ';
   static const String description = 'Description: ';
-
-
-
-  
 }
